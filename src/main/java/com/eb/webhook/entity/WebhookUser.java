a8763cb7b92fdc4bc328c2245e8a92259f7e81ae
yipan-easybling <yipan@easybilling.cloud>
2026-05-13 16:00:51 +0800

    Add WebhookUser entity and update workspace configuration

---
+++ src/main/java/com/eb/webhook/entity/WebhookUser.java
@@ -0,0 +1,13 @@
+package com.eb.webhook.entity;
+
+public class WebhookUser {
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
